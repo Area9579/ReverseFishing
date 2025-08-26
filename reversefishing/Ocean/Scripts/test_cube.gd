88b6d2ecbd495232ba9934e8c35d5c0f76ba73c8
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-08-26 15:28:00 -0500

    WORKING BUOYANCY

--- reversefishing/Ocean/Scripts/test_cube.gd
+++ reversefishing/Ocean/Scripts/test_cube.gd
@@ -5,20 +5,16 @@ extends RigidBody3D
 @export var angular_water_drag : float = 0.1
 
 @onready var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var water: Water = $"../Water"
-
-const water_height : float = 0.0
+@onready var water = $"../Water2"
 
 var submerged : bool = false
 
-## re-used local variables for _physics_process(): (defining them here saves allocation time)
-var depth : float
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	depth = water.get_height(global_position) - global_position.y
+	var depth = water.get_height(global_position) - global_position.y
 	if depth > 0:
 		apply_central_force(Vector3.UP * float_force * gravity * depth)
 		submerged = true
